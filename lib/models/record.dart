53e07ba6366c2f0d47e3e812c23288f6db65b79c
FlutLab.io <>
2022-08-02 22:17:55 +0000

    Autosave

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -13,3 +13,27 @@ class Record extends HiveObject {
     this.createdAt,
   });
 }
+
+class RecordAdapter extends TypeAdapter<Record> {
+  @override
+  final typeId = 0;
+
+  @override
+  Record read(BinaryReader reader) {
+    var numOfFields = reader.readByte();
+    var fields = <int, dynamic>{
+      for (var i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Record(
+      word: reader.read() as String,
+      createdAt: reader.read() as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Record obj) {
+    writer
+      ..write(obj.word)
+      ..write(obj.createdAt);
+  }
+}
